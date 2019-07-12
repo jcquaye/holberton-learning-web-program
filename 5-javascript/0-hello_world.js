12fc2c66382d31c2af82dbd062c527485fb7b382
julian quaye <jkquaye@gmail.com>
2019-07-12 10:13:39 -0700

    h

---
+++ 5-javascript/0-hello_world.js
@@ -0,0 +1,6 @@
+console.log("hello world")
+VM121:1 hello world
+undefined
+console.log("Betty Holberton")
+VM199:1 Betty Holberton
+undefined
---
+++ 5-javascript/3-my_first_loop.js
@@ -0,0 +1,6 @@
+let i = 0
+
+while(i < 11) {
+    console.log("<p>The number is " + i + "</p>");
+    i++;
+}
